4e5f31acd82ea0dfc85287ae993b9edb94dd3100
Ahmad34-90 <meducation220@gmail.com>
2025-11-28 14:05:52 +0500

    Initial django TODO-List commit

---
+++ ToDo_site/ToDo_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from.models import Task
+# Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name= "tasks"
+    template_name = "ToDo_app/task_detail.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ["title","description","complete"] 
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields= ["title", "description","complete"]
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = "task"
+    success_url = reverse_lazy("tasks")
