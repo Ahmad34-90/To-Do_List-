a914062f0d72bfd25cfa6c9ac3f4b40ee63767c5
Ahmad34-90 <meducation220@gmail.com>
2025-11-28 15:27:56 +0500

    Adding Logout Functioality and redirecting with POST method.

--- ToDo_site/ToDo_app/urls.py
+++ ToDo_site/ToDo_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
-
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("",TaskList.as_view() , name="tasks"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("task-update/<int:pk>", TaskUpdate.as_view(), name="task-update"),
     path("task-confirm-delete/<int:pk>", TaskDelete.as_view(), name="task-confirm-delete"),
     path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
